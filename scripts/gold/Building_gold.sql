6e1bac427eb74e024ea5dd3753c5236381ff00f3
freudyfoy <freudyfoy555@gmail.com>
2026-02-04 13:53:35 +0700

    Add files via upload

---
+++ scripts/gold/Building_gold.sql
@@ -0,0 +1,105 @@
+
+-- ##################################################################################################
+-- ##################################################################################################
+-- Dimension customers
+
+CREATE VIEW gold.dim_customers AS 
+--SELECT cst_id, COUNT(*) FROM (	-- Check if it duplicate after join tables
+SELECT
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,	-- Surrogate key
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr	-- CRM is the master source for gender info
+		 ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la
+ON ci.cst_key = la.cid;
+--)t GROUP BY cst_id
+--HAVING COUNT(*) > 1
+
+
+-- ##################################################################################################
+-- ##################################################################################################
+
+-- Data integration between ci.cst_gndr and ca.gen
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr	-- CRM is the master source for gender info
+		 ELSE COALESCE(ca.gen, 'n/a')
+	END AS new_gen
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+
+-- ##################################################################################################
+-- ##################################################################################################
+-- Dimension Products
+
+CREATE VIEW gold.dim_products AS
+--SELECT prd_id, COUNT(*) FROM (	-- Check if it duplicate after join tables
+SELECT
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,	
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS sub_category,
+	pc.maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info as pn
+LEFT JOIN silver.erp_px_cat_g1v2 as pc
+ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL		-- filter out all historical data
+--)t GROUP BY prd_id
+--HAVING COUNT(*) > 1 
+
+
+-- ##################################################################################################
+-- ##################################################################################################
+
+-- Check date data is valid 
+-- => if the end date is NULL then it's current info of the product
+select distinct
+pn.prd_end_dt
+FROM silver.crm_prd_info as pn
+
+
+-- ##################################################################################################
+-- ##################################################################################################
+
+-- Building Fact: Use the dimension's surrogate keys instead of IDs to easily connect facts with dimensions
+-- Fact Sales
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+	sd.sls_ord_num AS order_number,
+	pr.product_key,
+	cu.customer_id,
+	sd.sls_order_dt AS order_date,
+	sd.sls_ship_dt AS shipping_date,
+	sd.sls_due_dt AS due_date,
+	sd.sls_sales AS sales_amount,
+	sd.sls_quantity AS quantity,
+	sd.sls_price
+FROM silver.crm_sales_details AS sd
+LEFT JOIN gold.dim_products AS pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers AS cu
+ON sd.sls_cust_id = cu.customer_id
+
+
+-- ##################################################################################################
+-- ##################################################################################################
